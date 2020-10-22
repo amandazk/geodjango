6c249bbc392ffcd06348eb2b2c1b8fbc2009ce22
amandazk <zacharukaz@gmail.com>
2020-10-22 17:25:40 -0300

    Mostrando mapa no admin

--- reporter/models.py
+++ reporter/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.contrib.gis.db import models
+from django.db.models import Manager as GeoManager
 
 class Incidences(models.Model):
-    name = models.CharField(max_length=20)
-    location = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
+    location = models.PointField(srid=4326)
+    objects = GeoManager()
 
     def __unicode__(self):
         return self.name    
