bd5355c195e48c961caa1cc79d0185df8ec02f68
amandazk <zacharukaz@gmail.com>
2020-10-22 16:33:38 -0300

    Configuração do banco de dados

--- agricom/settings.py
+++ agricom/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'agricom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'agricom',
+        'USER': 'postgres',
+        'HOST': 'localhost:5432'
     }
 }
 
