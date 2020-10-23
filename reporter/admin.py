f55c9af9c55465c77474493e9fdddfc3d49615f8
amandazk <zacharukaz@gmail.com>
2020-10-23 17:29:34 -0300

    PolygonField

--- reporter/admin.py
+++ reporter/admin.py
@@ -3,7 +3,7 @@ from .models import Incidences
 from leaflet.admin import LeafletGeoAdmin
 
 class IncidencesAdmin(LeafletGeoAdmin):
-    list_display = ('name', 'location')
+    list_display = ('name', 'location', 'geometry')
 
 
 admin.site.register(Incidences, IncidencesAdmin)
--- reporter/models.py
+++ reporter/models.py
@@ -5,13 +5,12 @@ from django.db.models import Manager as GeoManager
 class Incidences(models.Model):
     name = models.CharField(max_length=100)
     location = models.PointField(srid=4326)
-    objects = GeoManager()
-
-    def __unicode__(self):
-        return self.name    
+    objects = GeoManager()  
+    geometry = models.PolygonField(srid=4326, null=True, geography=True)
 
     class Meta:
         verbose_name_plural = "Incidences"
 
     def __str__(self):
-        return self.name 
+        return self.name
+
